259a51d18a945e4c419e491af6b25b3684752d07
sepanta hosseini <HosseiniSepanta05@gmail.com>
2026-08-30 17:52:32 +0000

    Getting responses

--- CODES/weather_forecast.py
+++ CODES/weather_forecast.py
@@ -2,18 +2,33 @@ import pandas as pd
 import requests
 
 city = input("Enter your city: ")
-url = f"https://geocoding-api.open-meteo.com/v1/search?name={city}"
+url_city = f"https://geocoding-api.open-meteo.com/v1/search?name={city}"
+response = requests.get(url=url_city)
 
-response = requests.get(url=url)
-
+# Getting status code from the response
 status = response.status_code
-
 if status == 200:
-    print(f"Status: {status}")
+    print(f"City Response Status: {status}")
 elif status == 403:
-    print(f"Status:{status} \nThe request was declined")
+    print(f"City Response Status:{status} \nThe request was declined")
 
+# Getting json data from the response
 json = response.json()
 
-print(json)
+# Getting latitude and longitude from the json data
+latitude = json['results'][0]['latitude']
+longitude = json['results'][0]['longitude']
+
+url_weather = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}"
+response_weather = requests.get(url=url_weather)
+
+# Getting status code from the response
+if response_weather.status_code == 200:
+    print(f"Weather Response Status: {response_weather.status_code}")
+elif response_weather.status_code == 403:
+    print(f"Weather Response Status:{response_weather.status_code} \nThe request was declined")
+
+json_weather = response_weather.json()
+
+print(json_weather)
 
