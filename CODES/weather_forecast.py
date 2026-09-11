6fff32cb6eeafb10e45246ec7e8d366e65e376d8
sepanta hosseini <HosseiniSepanta05@gmail.com>
2026-09-11 18:46:45 +0000

    Make get weather description

--- CODES/weather_forecast.py
+++ CODES/weather_forecast.py
@@ -1,6 +1,37 @@
 import requests
 import sys
 
+weather_descriptions = {
+    0: "Clear sky ☀️",
+    1: "Mainly clear 🌤️",
+    2: "Partly cloudy ⛅",
+    3: "Overcast ☁️",
+    45: "Fog 🌫️",
+    48: "Depositing rime fog 🌫️",
+    51: "Light drizzle 🌦️",
+    53: "Moderate drizzle 🌦️",
+    55: "Dense drizzle 🌧️",
+    56: "Light freezing drizzle 🧊🌧️",
+    57: "Dense freezing drizzle 🧊🌧️",
+    61: "Slight rain 🌧️",
+    63: "Moderate rain 🌧️",
+    65: "Heavy rain 🌧️",
+    66: "Light freezing rain 🧊🌧️",
+    67: "Heavy freezing rain 🧊🌧️",
+    71: "Slight snow 🌨️",
+    73: "Moderate snow 🌨️",
+    75: "Heavy snow ❄️",
+    77: "Snow grains ❄️",
+    80: "Slight rain showers 🌦️",
+    81: "Moderate rain showers 🌦️",
+    82: "Violent rain showers ⛈️",
+    85: "Slight snow showers 🌨️",
+    86: "Heavy snow showers ❄️",
+    95: "Thunderstorm ⛈️",
+    96: "Thunderstorm with slight hail ⛈️🧊",
+    99: "Thunderstorm with heavy hail ⛈️🧊"
+}
+
 def get_requests(url):
     """
     Sends a GET request to the specified URL and returns the response.
@@ -66,6 +97,15 @@ def current_weather(current, current_units):
         wind_speed,\
         weather_code
 
+def get_weather_description(weather_code):
+    """
+    Returns a description of the weather based on the weather code.
+    """
+    if weather_code in weather_descriptions:
+        return weather_descriptions[weather_code]
+    else:
+        return "Unknown weather code"
+
 '''
     f"Current Weather:\
 \n🌡 Temperature: {temperature}\
@@ -92,18 +132,31 @@ response_weather = get_weather_data(latitude, longitude)
 status_code("Weather", response_weather)
 
 weather_data = response_weather.json()
+daily = weather_data["daily"]
+
+dates = daily["time"]
+max_temperatures = daily["temperature_2m_max"]
+min_temperatures = daily["temperature_2m_min"]
+precipitation_sums = daily["precipitation_sum"]
+weather_codes = daily["weather_code"]
+
+for i in range(len(dates)):
+    print(f"Date: {dates[i]}\
+        \nMax Temperature: {max_temperatures[i]}°C\
+        \nMin Temperature: {min_temperatures[i]}°C\
+        \nPrecipitation: {precipitation_sums[i]} mm\
+        \nWeather Code: {get_weather_description(weather_codes[i])}\n")
+
 temperature, humidity, precipitation, wind_speed, weather_code = current_weather(
     weather_data["current"],
     weather_data["current_units"]
 )
 
-print(weather_data)
-
-"""
+'''
 print(f"Current Weather:\
 \n🌡 Temperature: {temperature}\
 \n💧 Humidity: {humidity}\
 \n🌧 Precipitation Probability: {precipitation}\
 \n💨 Wind Speed: {wind_speed}\
 \n🌤 Weather Code: {weather_code}")
-"""
+'''
